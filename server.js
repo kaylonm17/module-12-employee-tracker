db8da66346198d1e56e0daa7a61fad4bbf38f6ed
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-03-24 12:05:06 -0500

    working on server.js

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const express = reqire('express')
 const mysql = reqire('mysql2')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // 
+        user: 'root',
+
+        password: 'Texans17',
+        database: 'employeelist_db'
+    },
+    console.log(`Connected tothe employeelist_db database.`)
+);
+
