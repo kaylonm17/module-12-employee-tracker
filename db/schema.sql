7c2761be31d44530b72ea5c4aefe17c363cf1bae
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-03-23 23:57:47 -0500

    created schema tables

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,14 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL
+    salary DECIMAL,
     department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
 )
