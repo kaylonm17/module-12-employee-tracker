680e81c12557d28369638b5b5517539036eaef51
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-04-02 20:18:46 -0500

    added async function addDepartment

--- index.js
+++ index.js
@@ -89,3 +89,34 @@ async function init() {
 // findAllDeparments()code is in the db/index.js
 // findAllDeparments() returns an array of db data rows
 // console.table() takes the rows and formants the output with column titles and data
+async function viewDepartments() {
+    await db
+        .findAllDepartments()
+        .then(([rows]) => {
+            let departments = rows;
+            console.log('\n');
+            console.log('Deparments Table');
+            console.table(departments);
+        })
+        .then(() => init());
+}
+
+// Add a department
+async function addDepartment() {
+    // gets the name of the department from the user
+    await prompt([
+        {
+            name: 'name',
+            message: 'What is the name of the department?',
+        },
+    ]).then((res) => {
+        let name = res;
+        db.createDepartment(name)
+            .then(() => {
+                console.log('\n\n\n');
+                console.log(`Added ${name.name} to the database`);
+                console.log(`View All Departments to see the result`);
+            })
+            .then(() => init())
+    });
+}
