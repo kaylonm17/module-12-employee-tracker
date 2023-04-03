1d2ed2edc5a25237ac8b74539923c7e1080b8071
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-04-02 21:40:46 -0500

    added more classes to db/index

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,8 @@
 const mysql = reqire('mysql2');
 
-// Connect to database
 const connection = mysql.createConnection({
     
         host: 'localhost',
-        // 
         user: 'root',
         password: 'Texans17',
         database: 'employeelist_db'
--- db/index.js
+++ db/index.js
@@ -23,12 +23,33 @@ class DB {
     async findAllRoles() {
         return  db_connection.promise().query('SELECT * FROM role');
     }
-   
+    // create role
+    async createRole(role){
+        returndb_connection.promise().query('SELECT * FROM employee');
+        return data;
+    }
+    // create employee
+    async createEmployee(employee) {
+        let data = await db_connection
+            .promise()
+            .query('INSERT INTO employee SET ?', employee);
+        return data;
+    }
+    // find all employee
     async findAllEmployees() {
         let data = await db_connection.promise().query('SELECT * FROM employee');
         return data;
     }
-    
+    //update employee role
+    async updateEmployeeRole(employeeId, roleId) {
+      let data = await db_connection
+        .promise()
+        .query('UPDATE employee SET role_id = ? WHERE id = ?', [
+          roleId,
+          employeeId,
+        ]);
+      return data;
+    }
 } 
 // end class
 module.exports = new DB(db_connection); 
